49838725b2130932e60f327cac36de39af45db26
NdegwaJulius <juliusndegwa2@gmail.com>
2022-10-11 07:51:03 +0300

    Setting navigation routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chatting_app/screens/chat_screen.dart';
+import 'package:flutter_chatting_app/screens/login_screen.dart';
+import 'package:flutter_chatting_app/screens/registration_screen.dart';
+import 'package:flutter_chatting_app/screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
+          bodyText1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => WelcomeScreen(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/second': (context) =>  LoginScreen(),
+        '/third': (context) =>  RegistrationScreen(),
+        '/fourth': (context) =>  ChatScreen(),
+      },
     );
   }
 }
